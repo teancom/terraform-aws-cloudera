df7cc8a4e7bac985954b3eb6e2b3559d2f5c62af
David Bishop <david@gnuconsulting.com>
2014-11-06 14:30:39 -0500

    add aws_key_path to variables.tf

--- variables.tf
+++ variables.tf
@@ -3,6 +3,7 @@ variable "network" {}
 variable "aws_route_table_private" {}
 variable "aws_centos_ami" {}
 variable "aws_key_name" {}
+variable "aws_key_path" {}
 variable "aws_subnet_bastion" {}
 variable "hadoop_instance_count" {
         default = "5"
